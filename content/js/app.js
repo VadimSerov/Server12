e3dde26c1868faf97ba2de8732751d7e4a8889ae
VadimSerov <vadimserov1964@gmail.com>
2019-12-07 18:13:09 +0500

    git init

---
+++ content/js/app.js
@@ -0,0 +1,52 @@
+const socket = io();
+
+t001.innerHTML = "окно загружено ";
+but01.onclick = function(){
+	t001.innerHTML = "Жесть от Node JS";
+}
+function emitToServer(){
+	socket.emit("eventServer",input.value);
+}
+send.onclick = emitToServer;
+input.onchange = emitToServer;
+socket.on("eventClient",function(msg){
+	t002.innerHTML += msg.data + "<br>";
+	input.value = "";
+});
+//кнопка Прочитать файл
+btnfnread.onclick = function(){
+	socket.emit("fnServer",filenameread.value);
+}
+//вдруг что то пришло с сервера
+socket.on("fnClient",function(dann){
+	filenamewrite.value = dann.filename ;
+	if(dann.err == ""){
+		txtar.value = dann.data ;
+	} else {
+		txtar.value = dann.data ;
+		t002.innerHTML += "Error: " + dann.err + " " + dann.filename + "<br>";
+		input.value = "";
+	}
+});
+//Кнопка записать в файл
+btnfnwrite.onclick = function(){
+	socket.emit("wrServer",{"filename":filenamewrite.value,"data":txtar.value});
+}
+//ответ с сервера как прошла запись
+socket.on("wrClient",function(datt){
+	if(datt.err != ""){
+		t002.innerHTML += "Error: " + datt.err + " " + datt.filename + "<br>";
+		input.value = "";
+	}
+});
+//задача file2 -- 1000 задач по программированию Часть II Абрамян М.Э. 2004 --
+//решение на стороне сервера, бинарные файлы не использовать
+btnfile2.onclick = function(){
+	socket.emit("file2Server",{"n":Number(inpn.value),"filename":inpfilename.value});
+}
+//задача file2 -- 1000 задач по программированию Часть II Абрамян М.Э. 2004 --
+//решение на стороне сервера, бинарные файлы не использовать, 
+//обязательно использовать парсинг в JSON 
+btnfile3.onclick = function(){
+	socket.emit("file3Server",{"a":Number(inpa.value),"b":Number(inpb.value),"filename":inpfilename3.value});
+}
